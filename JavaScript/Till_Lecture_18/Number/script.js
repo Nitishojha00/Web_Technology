afc0534404c99074d90bd89c06a471e0a40db7a8
Nitishojha00 <nitishojha00@gmail.com>
2025-09-03 22:16:35 +0530

    updated coa unit 2 IEEE 64 method that used in js to store number

--- JavaScript/Till_Lecture_18/Number/script.js
+++ JavaScript/Till_Lecture_18/Number/script.js
@@ -6,3 +6,12 @@ console.log(n.toPrecision(2));
 console.log(isNaN(n)); 
 
 console.log(isFinite(n)); 
+
+// how number stored in javascript i studied  ieee-64 bit  in semester COA Computer Organization And Architecture
+
+// 0-63  ->  total Bits
+// 0-52  ->  mentissa
+// 53-62 -> exponent
+// 63 -> set bit -> number is negative or positive
+
+// number jo bhi hooga usko binary form me convert kro 1.___*2^x form me convert kro ab point ke bad wala ko mentissa me likho and jo 2 ke power hai usko 1023 me add krke convert kro binary number me jo exponent me show hoga
