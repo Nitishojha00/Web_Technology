daeee97a1914be75cb437a5f2a5aa58d413b1e3b
Nitishojha00 <nitishojha00@gmail.com>
2025-09-04 21:25:27 +0530

    completed bro i can add more think that it precompute if match going to draw i will add later

--- JavaScript/Lecture_26/TIC TAC TOE/script.js
+++ JavaScript/Lecture_26/TIC TAC TOE/script.js
@@ -1,5 +1,3 @@
-let turn = 0;
-const board = document.getElementById('board');
 let arr = [["B","B","B"],["B","B","B"],["B","B","B"]];
 function checkWinner() {
     // Check rows
@@ -27,47 +25,68 @@ function checkWinner() {
     // No winner
     return false;
 }
-
-
+let turn = 0;
+let cnt = 0;
+const board = document.getElementById('board');
 const result = document.getElementById('result');
-  board.addEventListener("click",(e)=>{
-            const str = e.target.className;
-            let box = Number(str.substring(0,4)); // konsa wala box hai pata chal jayega
+function printer(e)
+    {
+            let str = e.target.className;
             let boxNum = Number(str.substring(3,4)); // konsa wala box hai pata chal jayega
             let row = Math.floor(boxNum/3);
             let col = Math.floor(boxNum%3);
+
+            const box = document.querySelector(`.${str}`);
             
-            if(arr[row][col]!='B') 
+            if(arr[row][col]==='B') 
             {
-                
-            }
-            else
-            {
-                
+                cnt++;   
                 if(turn===0)
                 {
-                    arr[row][col] = "0";
-                    box.textContent = "O";
+                    arr[row][col] = "O";
+                    box.innerText = "O";
                     turn = 1;
                     if(checkWinner())
                     {
                         result.textContent = "O Wins";
                         // jitgya ab remove krdo event listener
-                        board.removeEventListener("click",board);
+                        board.removeEventListener("click",printer);
                     }
                 }
                 else
                 {
                     arr[row][col] = "X";
-                    box.textContent = "X";
+                    box.innerText = "X";
                     turn = 0;
                     if(checkWinner())
                     {
                         result.textContent = "X Wins";
                         // jitgya ab remove krdo event listener
-                        board.removeEventListener("click",board);
+                       board.removeEventListener("click",printer);
                     }
                 }
             }
+        if(cnt==9)
+        {
+            result.textContent = "Match Draw";
+            board.removeEventListener("click",printer);
+        }
+    }
 
-    });
+board.addEventListener("click",printer);
+const btn = document.querySelector('button');
+btn.addEventListener('click',(e)=>{
+    // reset krdiya
+        arr =  [["B","B","B"],["B","B","B"],["B","B","B"]];
+        for(let child of board.childNodes)
+        {
+            child.textContent = "";
+        }
+        result.textContent = "";
+        turn = 0;
+        cnt = 0;
+        board.addEventListener("click", printer);
+});
+
+// i can also add some cunctionalitiies that tell game 
+// is drawn or not 
